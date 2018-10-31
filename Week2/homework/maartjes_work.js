f245d445bfbd8ebe2b54ccb26b9515dab0a14cb0
BraianT <42869449+BraianT@users.noreply.github.com>
2018-10-31 14:41:57 -0400

    week2 js HW
    completed

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -45,3 +45,19 @@ const tuesday = [
 const tasks = monday.concat(tuesday);
 
 // Add your code here
+/*const hoursPayment = tasks.map(function (hours) {
+  return hours.duration / 60  // takes all values of duration and converts them into Hours
+}) .filter(function(hours) {
+  return hours >= 2   // filters tasks of 2 hours or longer
+}) .map(function(hours) {
+  return hours * 125 / 10 // multiply to SEK value, and then divedes by Euro value
+}) .reduce(function(sum, hours) {
+  return sum+= hours
+}, 0) // gets the total earned in euros
+*/
+const hoursPayment = tasks.map( (hours)=>  hours.duration / 60 ) 
+.filter((hours) => hours >= 2 ) 
+.map((hours) =>  hours * 125 / 10 ) 
+.reduce((sum, hours) => sum+= hours, 0) 
+
+console.log(hoursPayment)
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,22 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+/* 
+const newNumbers = numbers.filter(function(odd){
+    return odd % 2 !== 0 // goes through numbers array and filters odd numbers
+} ).map(function(odd) {
+    return odd *2 // goes through  the odd numbers and multipies them times 2 
+}) */
+
+//arrow function
+const newNumbers = numbers.filter((odd) => odd % 2 !== 0 ) .map ((odd) => odd *2)
+
+/*const newNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 !== 0) {
+    newNumbers.push(numbers[i] * 2);
+  }
+}
+*/
+console.log('The doubled numbers are', newNumbers); // ==> [2, 6]
